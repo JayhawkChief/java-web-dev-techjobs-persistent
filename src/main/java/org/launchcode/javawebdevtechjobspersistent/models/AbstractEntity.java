4a2778259d8248e3eeb4a1067d373a6e5949c43d
JayhawkChief <bxrayhamm@gmail.com>
2020-03-07 16:47:36 -0600

    First commit of the assignment

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,12 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotNull(message = "Name may not be blank!")
     private String name;
 
     public int getId() {
